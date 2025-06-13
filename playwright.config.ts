d11820524e036eb127ab99adcc9ca608ac09c2e4
chan.song <chan.song@swarobo.ai>
2025-06-13 10:28:57 +0900

    playwrite config update

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,11 +3,12 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
+  timeout: 10 * 1000, // Set overall test timeout to 10 seconds
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   use: {
     baseURL: 'http://localhost:3000', // Ensure your dev server runs on this port
     trace: 'on-first-retry',
@@ -30,5 +31,8 @@ export default defineConfig({
     command: 'npm run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
+    timeout: 10 * 1000, // Give the web server 60 seconds to start
+    stderr: 'pipe', // Capture stderr for debugging
+    stdout: 'pipe', // Capture stdout for debugging
   },
 });

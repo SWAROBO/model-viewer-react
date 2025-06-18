619966787dd53453b064a60d280d1297370a3a7e
chan.song <chan.song@swarobo.ai>
2025-06-18 19:40:44 +0900

    feat: restore and enhance CI workflow by re-enabling coverage checks and improving Playwright test report handling

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,38 +1,43 @@
 // playwright.config.ts
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './e2e',
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: [['html', { open: 'never' }]],
-  timeout: 120000,
-  use: {
-    baseURL: 'http://localhost:3000', // Ensure your dev server runs on this port
-    trace: 'on-first-retry',
-    actionTimeout: 90 * 1000,
-    navigationTimeout: 90 * 1000,
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+    testDir: "./e2e",
+    outputDir: "playwright-report/test-results",
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: [
+        ["html", { open: "never", outputFolder: "playwright-report" }],
+        ["json", { outputFile: "playwright-report/results.json" }],
+    ],
+
+    timeout: 20000,
+    use: {
+        baseURL: "http://localhost:3000", // Ensure your dev server runs on this port
+        trace: "on-first-retry",
+        actionTimeout: 20 * 1000,
+        navigationTimeout: 20 * 1000,
     },
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-  ],
+    projects: [
+        {
+            name: "chromium",
+            use: { ...devices["Desktop Chrome"] },
+        },
+        // {
+        //   name: 'firefox',
+        //   use: { ...devices['Desktop Firefox'] },
+        // },
+        // {
+        //   name: 'webkit',
+        //   use: { ...devices['Desktop Safari'] },
+        // },
+    ],
     webServer: {
-    command: 'npx serve@latest out',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000, // Increase timeout for CI
-  },
+        command: "npx serve@latest out",
+        url: "http://localhost:3000",
+        reuseExistingServer: !process.env.CI,
+        timeout: 120 * 1000, // Increase timeout for CI
+    },
 });

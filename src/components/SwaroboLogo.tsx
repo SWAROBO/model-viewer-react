001d00e745995cd4d87cd8806a528a5b727f6b02
chan.song <chan.song@swarobo.ai>
2025-08-01 15:34:55 +0900

    LCP fixed

--- src/components/SwaroboLogo.tsx
+++ src/components/SwaroboLogo.tsx
@@ -5,7 +5,7 @@ const SwaroboLogo: React.FC = () => {
     return (
         <div className="logo-container">
             <a href="https://swarobo.ai/" target="_blank" rel="noopener noreferrer">
-                <Image src="/logo-swarobo.png" alt="SWAROBO Logo" className="swarobo-logo" width={100} height={50} />
+                <Image src="/logo-swarobo.png" alt="SWAROBO Logo" className="swarobo-logo" width={100} height={50} priority />
             </a>
         </div>
     );
